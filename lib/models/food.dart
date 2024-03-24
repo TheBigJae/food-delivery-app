1e9e2a7920e4446764f2e263d43e2801f5fd4208
TheBigJae <johnbamigbola10@gmail.com>
2024-03-24 23:08:52 +0100

    commit-105

---
+++ lib/models/food.dart
@@ -0,0 +1,37 @@
+//food item
+class Food {
+  final String name;
+  final String descriptioon;
+  final String imagePath;
+  final double price;
+
+  final FoodCategory category;
+  List<Addon> availableDdons;
+
+  Food(
+      {required this.name,
+      required this.descriptioon,
+      required this.imagePath,
+      required this.price,
+      required this.category,
+      required this.availableDdons});
+}
+
+//food category
+
+enum FoodCategory { burgers, salads, sides, deserts, drinks }
+
+//food addons
+
+class Addon {
+  late String name;
+  double price;
+
+
+  Addon({
+  required this.name,
+  required this.price
+});
+}
+
+
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:food_delivery_app/components/My_current_location.dart';
 import 'package:food_delivery_app/components/my_description_box.dart';
 import 'package:food_delivery_app/components/my_drawer.dart';
 import 'package:food_delivery_app/components/my_silver_app_bar.dart';
+import 'package:food_delivery_app/components/my_tab_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,16 +12,31 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+
+  //tab controller
+
+  late TabController _tabController;
   @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _tabController.dispose();
+    super.dispose();
+  }
+   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: const MyDrawer() ,
       body: NestedScrollView(
         headerSliverBuilder: (context, innerBoxIsScrolled)=>
         [MySliverAppBar(
-          title: const Text('title'
-           ),
+          title: MyTabBar(tabController: _tabController),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
@@ -36,9 +52,25 @@ class _HomePageState extends State<HomePage> {
               //Description Box
               const MyDescriptionBox(),
             ],
-          ))
+          )
+          )
            ],
-           body: Container(color: Colors.blue,),
+           body: TabBarView(
+            controller: _tabController,
+            children: [
+              ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("First Tab Item"),
+                ),
+                ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("Second Tab Item"),
+                ),
+                ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Text("Third Tab Item"),
+                ),
+           ]),  
       ),
     );
   }
