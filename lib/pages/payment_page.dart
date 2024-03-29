2c87f2a30b7c4cbfb8b6b0756ade4703d6a61eb7
TheBigJae <johnbamigbola10@gmail.com>
2024-03-29 06:30:05 +0100

    commit-111

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:food_delivery_app/components/my_button.dart';
+import 'package:food_delivery_app/pages/delivery_progress_page.dart';
 
 class PaymentPage extends StatefulWidget {
   const PaymentPage({super.key});
@@ -9,12 +11,51 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
-
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
   String cardNumber = '';
   String expiryDate = '';
   String cardHolderName = '';
   String cvvCode = '';
   bool isCvvFocused = false;
+
+  void userTappedPay() {
+    if (formKey.currentState!.validate()) {
+      //only show dialog box is form is validated
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text("Confirm Payment"),
+                content: SingleChildScrollView(
+                  child: ListBody(
+                    children: [
+                      Text("Card Number: $cardNumber"),
+                      Text("Expiry Date: $expiryDate"),
+                      Text("Card Holder name: $cardHolderName"),
+                      Text("CVV: $cvvCode"),
+                    ],
+                  ),
+                ),
+                actions: [
+                  //cancel button
+                  TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text("Cancel")),
+
+                  // Yes Button
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => DeliveryProgressPage()),
+                        );
+                      },
+                      child: const Text("Yes"))
+                ],
+              ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +76,34 @@ class _PaymentPageState extends State<PaymentPage> {
               cardHolderName: cardHolderName,
               cvvCode: cvvCode,
               showBackView: isCvvFocused,
-              onCreditCardWidgetChange: (p0){})
+              onCreditCardWidgetChange: (p0) {}),
+
+          //credit card form
+
+          CreditCardForm(
+              cardNumber: cardNumber,
+              expiryDate: expiryDate,
+              cardHolderName: cardHolderName,
+              cvvCode: cvvCode,
+              onCreditCardModelChange: (data) {
+                setState(() {
+                  cardNumber = data.cardNumber;
+                  expiryDate = data.expiryDate;
+                  cardHolderName = data.cardHolderName;
+                  cvvCode = data.cvvCode;
+                });
+              },
+              formKey: formKey),
+
+          const Spacer(),
+          MyButton(
+            text: "Pay Now",
+            onTap: userTappedPay,
+          ),
+
+          const SizedBox(
+            height: 25,
+          )
         ],
       ),
     );
