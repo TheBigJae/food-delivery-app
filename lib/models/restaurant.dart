12aaf497462ce7e0aae0919360a628d36c65f3c7
TheBigJae <johnbamigbola10@gmail.com>
2024-04-02 10:23:16 +0100

    done-with-UI

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/models/cart_item.dart';
+import 'package:intl/intl.dart';
 
 import 'food.dart';
 
@@ -355,7 +356,43 @@ class Restaurant extends ChangeNotifier {
 
   //generate a receipt
 
+  String displayCartReceipt() {
+    final receipt = StringBuffer();
+    receipt.writeln("Here's Your receipt.");
+    receipt.writeln();
+
+    //format date to include up to seconds only
+    String formattedDate =
+        DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
+
+    receipt.writeln(formattedDate);
+    receipt.writeln("-----------");
+
+    for (final cartItem in _cart) {
+      receipt.writeln(
+          "${cartItem.quantity} x ${cartItem.food.name} - ${_formatPrice(cartItem.food.price)}");
+      if (cartItem.selectedAddons.isNotEmpty) {
+        receipt.writeln(" Add-ons: ${_formatAddons(cartItem.selectedAddons)}");
+      }
+      receipt.writeln();
+    }
+    receipt.writeln("---------------");
+    receipt.writeln();
+    receipt.writeln("Total Items: ${getTotalItemCount()}");
+    receipt.writeln("Total Items: ${_formatPrice(getTotalPrice())}");
+
+    return receipt.toString();
+  }
+
   //format double value into money
+  String _formatPrice(double price) {
+    return "\$${price.toStringAsFixed(2)}";
+  }
 
   //format list of addons into a string summary
+  String _formatAddons(List<Addon> addons) {
+    return addons
+        .map((addon) => "${addon.name} (${_formatPrice(addon.price)})")
+        .join(",");
+  }
 }
--- lib/pages/delivery_progress_page.dart
+++ lib/pages/delivery_progress_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_receipt.dart';
 
 class DeliveryProgressPage extends StatelessWidget {
   const DeliveryProgressPage({super.key});
@@ -8,6 +9,83 @@ class DeliveryProgressPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Delivery in Progress"),
+      ),
+      bottomNavigationBar: _buildBottomNavBar(context),
+      body: Column(
+        children: [
+          const MyReceipt(),
+        ],
+      ),
+    );
+  }
+
+  //Custom Bottom NavBar - Message ?Call Delivery Driver
+  Widget _buildBottomNavBar(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          )),
+      padding: const EdgeInsets.all(25),
+      child: Row(
+        children: [
+          //profile pic of driver
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.background,
+              shape: BoxShape.circle,
+            ),
+            child: IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
+          ),
+          //driver details
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Mitch Koko ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+              Text(
+                "Driver",
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              )
+            ],
+          ),
+
+          const Spacer(),
+          Row(
+            children: [
+              //message button
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background,
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.message), color: Theme.of(context).colorScheme.primary,),
+              ),
+
+              const SizedBox(width: 10,),
+              //call button
+
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background,
+                  shape: BoxShape.circle,
+                ),
+                child: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.call), color: Colors.green,),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
